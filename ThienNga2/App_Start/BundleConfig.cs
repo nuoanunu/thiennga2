90f7d26a05c5aa7b4ff9bf57cce929fcecd95e79
nuoanunu <dwarpro@gmail.com>
2016-07-07 14:27:39 +0700

    Update source
    up up

--- ThienNga2/App_Start/BundleConfig.cs
+++ ThienNga2/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace ThienNga2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
