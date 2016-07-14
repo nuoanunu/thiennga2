c7a6d925e045113d7e79def9b54da649bc182464
nuoanunu <dwarpro@gmail.com>
2016-07-14 22:05:19 +0700

    xxczxc
    zxc

--- ThienNga2/Controllers/InventoryController.cs
+++ ThienNga2/Controllers/InventoryController.cs
@@ -11,7 +11,7 @@ using Twilio;
 namespace ThienNga2.Areas.Admin.Controllers
 {
     [Authorize(Roles ="admin")]
-    [Authorize(Roles = "InventoryController")]
+    [Authorize(Roles = "InventoryManager")]
     public class InventoryController : Controller
     {
         private ThienNgaDatabaseEntities am = new ThienNgaDatabaseEntities();
